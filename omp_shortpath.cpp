e0cfab69fb36dd3b5b00b3b83abdeed00b2aae4b
BrianJayD <brian.jay1795@gmail.com>
2019-04-04 16:45:18 -0400

    [Modified] Reads, parses and outputs data from file.

--- omp_shortpath.cpp
+++ omp_shortpath.cpp
@@ -1,6 +1,44 @@
 #include "omp_shortpath.h"
 
 int main(int argc, char const *argv[]) {
-  cout << "Hello\n";
+
+  ifstream infile(argv[1]);
+
+  string line, n, d;
+  string node_delim = ":";
+
+  while(getline(infile, line)) {
+    int delim_index = line.find(node_delim);
+    n = line.substr(0, delim_index);
+    d = line.substr(delim_index + 1, line.length());
+
+    cout << "NODE " << n << ":" << endl;
+
+    getNeighbors(d);
+
+  }
+  infile.close();
+
   return 0;
+}
+
+void getNeighbors(string data) {
+  //cout << data << endl << endl;
+  string line;
+  char delim = ',';
+  stringstream ss(data);
+
+  cout << "Dest.\tCost" << endl;
+
+  while(getline(ss, line, delim)) {
+    char nd = '-';
+    int nd_index = line.find(nd);
+    string nn = line.substr(0, nd_index);
+    string cost = line.substr(nd_index + 1, line.length());
+
+    cout << nn << "\t" << cost << endl;
+
+  }
+  cout << endl;
+
 }
--- omp_shortpath.h
+++ omp_shortpath.h
@@ -1,4 +1,14 @@
 #include <omp.h>
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <string>
 
 using namespace std;
+
+struct node {
+  string name;
+};
+
+void getNeighbors(string data);
