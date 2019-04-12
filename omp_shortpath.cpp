63639aa57d1483a43dfc6d7bf0dabc78eefc2ce2
BrianJayD <brian.jay1795@gmail.com>
2019-04-11 23:30:58 -0400

    [Modified] Implemented OpenMP.

--- omp_shortpath.cpp
+++ omp_shortpath.cpp
@@ -1,37 +1,91 @@
 #include "omp_shortpath.h"
 
+vector<string> visited;
+map<string, int> distances;
+vector<node> nodes;
+
+
 int main(int argc, char const *argv[]) {
+  omp_set_num_threads(NUM_THREADS);
+  int num_threads = omp_get_num_threads();
+
+  #pragma omp parallel for
+  for (int i = 0; i < NUM_THREADS; i++) {
+    #pragma omp critical
+    {
+      int thread_id = omp_get_thread_num();
+      cout << "THREAD ID: " << thread_id << endl;
+    }
+  }
 
   int numNodes = countLines(argv[1]);
 
-  node nodes[numNodes];
+  // BRIAN
+  // This takes starting node as arguement and changes to upper case for comparing
+  string startNode = argv[2];
+  transform(startNode.begin(), startNode.end(), startNode.begin(), ::toupper);
+  cout << "INPUT: " + startNode << endl;
 
   ifstream infile(argv[1]);
 
-  string line, n, d;
+  string line, name, paths;
   string node_delim = ":";
   int nodeIndex = 0;
+  int vecIndex = 0;
 
   while(getline(infile, line)) {
     int delim_index = line.find(node_delim);
-    n = line.substr(0, delim_index);
-    d = line.substr(delim_index + 1, line.length());
+    name = line.substr(0, delim_index);
+    paths = line.substr(delim_index + 1, line.length());
 
-    nodes[nodeIndex] = setNode(n, d);
+    nodes.push_back(setNode(name, paths));
+    distances.insert(pair<string, int>(name, 0));
+
+    // BRIAN
+    // This gets index of start node in node vector
+    if (startNode == name) {
+      vecIndex = nodeIndex;
+    }
+
     nodeIndex++;
   }
 
   infile.close();
 
+  cout << "Node being checked: " + startNode << endl;
+  visited.push_back(startNode);
 
-  map<string, int>::iterator itr;
+  node initialNode = nodes[vecIndex];
 
-  for (int y = 0; y < numNodes; y++) {
-    cout << "NODE: "<< nodes[y].name << endl;
+  // BRIAN
+  // Go through initial node paths and call updateDistances for each path
+  /*
+  for(itr = initialNode.paths.begin(); itr != initialNode.paths.end(); itr++) {
+    cout << "-----------------START-----------------" << endl << endl;
+    updateDistances(itr, initialNode.name, 0);
+    cout << "-----------------END-----------------" << endl << endl;
+  }
+  */
 
-    cout << "PATHS:" << endl;
-    for (itr = nodes[y].paths.begin(); itr != nodes[y].paths.end(); itr++) {
-      cout << itr->first << "\t" << itr->second << endl;
+  vector<string> adjNodes;
+  vector<int> adjDist;
+
+  map<string, int>::iterator mItr;
+  for(mItr = initialNode.paths.begin(); mItr != initialNode.paths.end(); mItr++) {
+    adjNodes.push_back(mItr->first);
+    adjDist.push_back(mItr->second);
+  }
+
+  #pragma omp parallel for shared(initialNode, adjNodes, adjDist)
+  for(int i = 0; i < initialNode.paths.size(); i++) {
+    //string nodeStr = initialNode.paths.at(i);
+    #pragma omp critical
+    {
+      cout << "THREAD ID  = " << omp_get_thread_num() << endl;
+      cout << adjNodes.at(i) << "\t" << adjDist.at(i) << endl;
+      cout << "-----------------START-----------------" << endl << endl;
+      updateDistances(adjNodes.at(i), adjDist.at(i), initialNode.name, 0);
+      cout << "-----------------END-----------------" << endl << endl;
     }
 
   }
@@ -40,27 +94,124 @@ int main(int argc, char const *argv[]) {
   return 0;
 }
 
-node setNode(string na, string data) {
+// BRIAN
+// Checks to see if node is visited to decide whether to update or add
+bool isVisited(string toCheck) {
+  vector<string>::iterator vecItr = find(visited.begin(), visited.end(), toCheck);
+  if(vecItr != visited.end()) {
+    cout << toCheck << " HAS BEEN VISITED" << endl;
+    return true;
+  }
+  cout << toCheck << " HAS NOT BEEN VISITED" << endl;
+  return false;
+}
 
-  node no;
-  no.name = na;
+// Brian
+// Tranverses through each path updating the distances map as nodes are visited
+void updateDistances(string key, int dist, string startNode, int d) {
+  int sum = dist + d;
+
+  cout << "START: "<< startNode << endl << "\t";
+
+  map<string, int>::iterator distItr = distances.find(key);
+
+  // BRIAN
+  // If node is not visited, add to distances map,
+  // else check if sum is less than recorded distance and update if true.
+  if(!isVisited(key)) {
+    visited.push_back(key);
+    if(distItr != distances.end()) {
+      cout << "ADDING " + key + " WITH DISTANCE " << sum << endl;
+      distItr->second = sum;
+    }
+  } else {
+    if(distItr->second > sum) {
+      cout << "UPDATING " + key + " WITH DISTANCE " << sum << endl;
+      distItr->second = sum;
+    } else {
+      cout << "NO UPDATE NEEDED (Sum=" << sum << " > Current=" <<
+        distItr->second << ")"<< endl;
+    }
+  }
+
+  // BRIAN
+  // Find visited node in vector
+  vector<node>::iterator nodeItr;
+  for(nodeItr = nodes.begin(); nodeItr != nodes.end(); nodeItr++) {
+    string toGo = (*nodeItr).name;
+
+    // BRIAN
+    // If the visited node is found in vector, go though paths of visited node
+    // and call updateDistances again to transverse through the available paths.
+    if(key == toGo) {
+      cout << toGo << endl;
+
+      // BRIAN
+      // Prints initial visited vector and distances map
+      cout << endl;
+      printVisited(visited);
+      cout << endl;
+      printMap(distances);
+
+      map<string, int>::iterator neighborItr;
+
+      // BRIAN
+      // Go through each path and start updateDistances function again
+      for(neighborItr = (*nodeItr).paths.begin(); neighborItr != (*nodeItr).paths.end(); neighborItr++) {
+        string newKey = neighborItr->first;
+        int newDist = neighborItr->second;
+        //cout << startNode << " to " << key << endl;
+        updateDistances(newKey, newDist, toGo, sum);
+      }
+    }
+  }
+
+}
+
+// BRIAN
+// Print map function
+void printMap(map<string, int> distMap) {
+  map<string, int>::iterator it;
+  cout << "UPDATED MAP: " << endl;
+  cout << "PATH\tDIST" << endl;
+  for(it = distMap.begin(); it != distMap.end(); it++) {
+    cout << it->first << "\t" << it->second << endl;
+  }
+}
+
+void printVisited(vector<string> vec){
+  cout << "PRINTNG VISITED" << endl;
+  for(int i = 0; i < vec.size();i++){
+    cout << vec[i] + ", ";
+  }
+  cout<< endl;
+}
+
+node setNode(string nodeName, string data) {
+
+  node n;
+  n.name = nodeName;
 
   string line;
   char delim = ',';
   stringstream ss(data);
   while(getline(ss, line, delim)) {
-    char nd = '-';
-    int nd_index = line.find(nd);
-    string nn = line.substr(0, nd_index);
-    string cost = line.substr(nd_index + 1, line.length());
-    no.paths.insert(pair<string, int>(nn, stoi(cost)));
-
+    char pathDelim = '-';
+    int nodeIndex = line.find(pathDelim);
+    string neighbor = line.substr(0, nodeIndex);
+    string cost = line.substr(nodeIndex + 1, line.length());
+    n.paths.insert(pair<string, int>(neighbor, stoi(cost)));
   }
 
-  return no;
+  return n;
 
 }
 
+/*
+Get the line count of the file we are reading to get the number of nodes
+@String file:file name
+@return c: integer of the amount of nodes in the algorithm
+*/
 int countLines(string file) {
 
   ifstream count(file);
--- omp_shortpath.h
+++ omp_shortpath.h
@@ -5,15 +5,35 @@
 #include <sstream>
 #include <string>
 #include <iterator>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
+
+#define NUM_THREADS 8
 
 struct node {
   string name;
   map<string, int> paths;
 };
 
+//this will hold the shortest path for each individual node
+struct Distance {
+  string name;
+  int path;
+};
 
-node setNode(string na, string data);
+bool isVisited(string toCheck);
+
+void updateDistances(string key, int dist, string startNode, int d);
+
+node setNode(string nodeName, string data);
 
 int countLines(string file);
+
+void printVisited(vector<string> vec);
+
+void printMap(map<string, int> distMap);
